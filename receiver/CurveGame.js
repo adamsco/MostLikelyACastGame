c77e2b4cde854f3b4d67160f84c6c1d2c61373ab
Jakob Svensson <jakob-_-92@hotmail.com>
2015-12-12 20:25:31 +0100

    Gamemanger should be set correctly now

--- receiver/CurveGame.js
+++ receiver/CurveGame.js
@@ -1,28 +1,28 @@
-var gameManager = null;
+
 
 function CurveGame(gameManager) {
-	gameManager = gameManager;
-	console.log('game manager set' + gameManager);
+	this.gameManager = gameManager;
+	console.log('game manager set' + this.gameManager);
 };
 
 CurveGame.prototype.onPlayerAvailable = function(event) {
 	console.log('Player ' + event.playerInfo.playerId + ' is available');
-	console.log('game manager in available' + gameManager);
+	console.log('game manager in available' + this.gameManager);
 	//If first player open lobby 
 	//Else check if lobby is open, if it is then add player to the lobby if it is not then tell player to wait for the next round
-	var availablePlayers = gameManager.getPlayersInState(cast.receiver.games.PlayerState.AVAILABLE, true);
+	var availablePlayers = this.gameManager.getPlayersInState(cast.receiver.games.PlayerState.AVAILABLE, true);
 	
 	
-	if(gameManager.getLobbyState()==cast.receiver.games.LobbyState.CLOSED){
+	if(this.gameManager.getLobbyState()==cast.receiver.games.LobbyState.CLOSED){
 		if(availablePlayers==1){//Player is first player in game so open lobby
-			gameManager.updateLobbyState(cast.receiver.games.LobbyState.OPEN, true);
-			gameManager.broadcastGameManagerStatus();
+			this.gameManager.updateLobbyState(cast.receiver.games.LobbyState.OPEN, true);
+			this.gameManager.broadcastGameManagerStatus();
 		}else{//A player tries to join when a game is running so tell the player to wait
 			//Send message to player that it needs to wait for current game to end and join the next
 		}		
 	}
 	
-	if(gameManager.getLobbyState()==cast.receiver.games.LobbyState.OPEN){
+	if(this.gameManager.getLobbyState()==cast.receiver.games.LobbyState.OPEN){
 		//Send message to player that it has joined the lobby
 		console.log('Lobby is open');
 	}
@@ -52,15 +52,15 @@ CurveGame.prototype.onLobbyOpen = function() {};
 CurveGame.prototype.onLobbyClosed = function() {};
 
 function checkIfAllReady(){
-	var readyPlayers = gameManager.getPlayersInState(cast.receiver.games.PlayerState.READY, true);
-	var availablePlayers = gameManager.getPlayersInState(cast.receiver.games.PlayerState.AVAILABLE, true);
+	var readyPlayers = this.gameManager.getPlayersInState(cast.receiver.games.PlayerState.READY, true);
+	var availablePlayers = this.gameManager.getPlayersInState(cast.receiver.games.PlayerState.AVAILABLE, true);
 	
 	if(availablePlayers.length == 0 && readyPlayers > 1){ //If no player is available and we have 2 or more ready players we can start the game
 		for (var i = 0; i < readyPlayers.length; i++) {
-			gameManager.updatePlayerState(readyPlayers.playerId,cast.receiver.games.PlayerState.PLAYING, true);
+			this.gameManager.updatePlayerState(readyPlayers.playerId,cast.receiver.games.PlayerState.PLAYING, true);
 		}
 	}
-	gameManager.broadcastGameManagerStatus();
+	this.gameManager.broadcastGameManagerStatus();
 	//Start game here
 	//Tell all players that the game has started
 };
