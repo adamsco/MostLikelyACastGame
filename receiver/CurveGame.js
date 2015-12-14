92f89f10c8f0fbe0c690d48e0c3ee52f439e1378
Jakob Svensson <jakob-_-92@hotmail.com>
2015-12-14 16:11:45 +0100

    One more fix
    soon it wil work

--- receiver/CurveGame.js
+++ receiver/CurveGame.js
@@ -84,9 +84,9 @@ CurveGame.prototype.checkIfAllReady = function(){
 	var availablePlayers = this.gameManager.getPlayersInState(cast.receiver.games.PlayerState.AVAILABLE);
 	
 	console.log('ready players: '+ readyPlayers.length);
-	console.log('available players' + availablePlayers);
+	console.log('available players' + availablePlayers.length);
 	
-	if(availablePlayers==undefined && readyPlayers.length >= 1){ //If no player is available and we have 2 or more ready players we can start the game
+	if(availablePlayers.length==undefined && readyPlayers.length >= 1){ //If no player is available and we have 2 or more ready players we can start the game
 		for (var i = 0; i < readyPlayers.length; i++) {
 			this.gameManager.updatePlayerState(readyPlayers.playerId,cast.receiver.games.PlayerState.PLAYING, true);
 		}
